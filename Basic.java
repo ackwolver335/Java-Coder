15365149951831ed88a20771c888141dd51450a5
ackwolver335 <abhaych335@gmail.com>
2024-04-02 20:57:00 +0530

    Starting Adding Java Basics

---
+++ Basic.java
@@ -0,0 +1,9 @@
+class Basic{
+    public static void main(String[] a){
+
+        // Starting with the Basics of the Program
+        // Typing the main text content on the screen
+        System.out.println("Hello World !");
+
+    }
+}
